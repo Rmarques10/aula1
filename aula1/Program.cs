4c0f60b5b2cf2cf9c2c9226cb509434ee625c369
rafin <rafin@DESKTOP-3JQ3N4B>
2020-06-01 21:16:10 -0300

    Add project files.

---
+++ aula1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            //i = "s";
+
+            var x = 2;
+
+            //x = "s";
+
+            string s = "Rafael";
+
+            Console.WriteLine(i);
+            Console.WriteLine(s);
+            Console.WriteLine(x);
+        }
+    }
+}
